826d37684fb2011c7f7a779843eec9583adf7822
fadyfouad <fady.fouad.a@gmail.com>
2019-05-24 23:46:50 +0200

    base

--- oldBase.ino
+++ oldBase.ino
@@ -1,7 +1,3 @@
-/*
- * rosserial PubSub Example
- * Prints "hello world!" and toggles led
- */
 
 #include <ros.h>
 #include <std_msgs/String.h>
@@ -11,19 +7,23 @@
 
 ros::NodeHandle  nh;
 
-#define MOTOR_LEFT_IN1 6
-#define MOTOR_LEFT_IN2 7
-#define MOTOR_LEFT_ENABLE 5
-#define MOTOR_RIGHT_IN1 10
-#define MOTOR_RIGHT_IN2 9
-#define MOTOR_RIGHT_ENABLE 8 
-int counter = 1 ;
-int speed_l = 0 ;
-int speed_r = 0 ;
-char *dir_l ;
-char *dir_r ;
-char *buf ;
-char *buf_s ;
+// motor one
+int MOTOR_F_RIGHT_ENABLE = 4;
+int MOTOR_F_RIGHT_IN1 = 3;
+int MOTOR_F_RIGHT_IN2 = 2;
+// motor two
+int MOTOR_F_LEFT_ENABLE = 7;
+int MOTOR_F_LEFT_IN1 = 6;
+int MOTOR_F_LEFT_IN2 = 5;
+// motor three
+int MOTOR_B_RIGHT_ENABLE = 10;
+int MOTOR_B_RIGHT_IN1 = 9;
+int MOTOR_B_RIGHT_IN2 = 8;
+// motor four
+int MOTOR_B_LEFT_ENABLE = 13;
+int MOTOR_B_LEFT_IN1 = 12;
+int MOTOR_B_LEFT_IN2 = 11;
+
 float v = 0.0 ;
 float w = 0.0 ;
 float vl = 0.0 ;
@@ -31,125 +31,132 @@ float vr = 0.0 ;
 
 std_msgs::UInt16 str_msg;
 ros::Publisher chatter("chatter", &str_msg);
+char hello[13] = "hello world!";
 
 void roverCallBack(const geometry_msgs::Twist& cmd_vel)
 {
-  v = cmd_vel.linear.x   ;
-  w = cmd_vel.angular.z  ;
-  vl = v - ((w*0.32)/2) ;
-  vr = v + ((w*0.32)/2) ;
-  vl = map(vl, -0.5, 0.5, -255, 255);
-  vr = map(vr, -0.5, 0.5, -255, 255);
-  if(v > 0 && w == 0){
-      analogWrite(MOTOR_LEFT_ENABLE,constrain(sqrt(sq(vl)),0,255));
-      digitalWrite(MOTOR_LEFT_IN1,HIGH);
-      digitalWrite(MOTOR_LEFT_IN2,LOW);
-      analogWrite(MOTOR_RIGHT_ENABLE,constrain(sqrt(sq(vr)),0,255));
-      digitalWrite(MOTOR_RIGHT_IN1,HIGH);
-      digitalWrite(MOTOR_RIGHT_IN2,LOW);   
-  }else if( v == 0 && w == 0){
-      analogWrite(MOTOR_LEFT_ENABLE,200);
-      digitalWrite(MOTOR_LEFT_IN1,HIGH);
-      digitalWrite(MOTOR_LEFT_IN2,HIGH);
-      analogWrite(MOTOR_RIGHT_ENABLE,200);
-      digitalWrite(MOTOR_RIGHT_IN1,HIGH);
-      digitalWrite(MOTOR_RIGHT_IN2,HIGH);  
-  }else if (v < 0 && w == 0){
-    analogWrite(MOTOR_LEFT_ENABLE,200);
-    digitalWrite(MOTOR_LEFT_IN1,LOW);
-    digitalWrite(MOTOR_LEFT_IN2,HIGH);
-    analogWrite(MOTOR_RIGHT_ENABLE,200);
-    digitalWrite(MOTOR_RIGHT_IN1,LOW);
-    digitalWrite(MOTOR_RIGHT_IN2,HIGH);
-  }else if (w > 0 && v == 0){
-      analogWrite(MOTOR_LEFT_ENABLE,constrain(sqrt(sq(vl)),0,255));
-      digitalWrite(MOTOR_LEFT_IN1,HIGH);
-      digitalWrite(MOTOR_LEFT_IN2,LOW);
-      analogWrite(MOTOR_RIGHT_ENABLE,constrain(sqrt(sq(vr)),0,255));
-      digitalWrite(MOTOR_RIGHT_IN1,HIGH);
-      digitalWrite(MOTOR_RIGHT_IN2,HIGH);
-    }else if (w < 0 && v == 0){
-      analogWrite(MOTOR_LEFT_ENABLE,constrain(sqrt(sq(vl)),0,255));
-      digitalWrite(MOTOR_LEFT_IN1,HIGH);
-      digitalWrite(MOTOR_LEFT_IN2,HIGH);
-      analogWrite(MOTOR_RIGHT_ENABLE,constrain(sqrt(sq(vr)),0,255));
-      digitalWrite(MOTOR_RIGHT_IN1,HIGH);
-      digitalWrite(MOTOR_RIGHT_IN2,LOW);
+    v = cmd_vel.linear.x   ;
+    w = cmd_vel.angular.z  ;
+    // vl = v - ((w * 0.32) / 2) ;
+    // vr = v + ((w * 0.32) / 2) ;
+    // vl = map(vl, -0.5, 0.5, -255, 255);
+    // vr = map(vr, -0.5, 0.5, -255, 255);
+
+    if (v > 0 && w == 0) {
+        analogWrite(MOTOR_F_LEFT_ENABLE, 255);
+        digitalWrite(MOTOR_F_LEFT_IN1, HIGH);
+        digitalWrite(MOTOR_F_LEFT_IN2, LOW);
+        analogWrite(MOTOR_F_RIGHT_ENABLE,255);
+        digitalWrite(MOTOR_F_RIGHT_IN1, HIGH);
+        digitalWrite(MOTOR_F_RIGHT_IN2, LOW);
+        analogWrite(MOTOR_B_LEFT_ENABLE, 255);
+        digitalWrite(MOTOR_B_LEFT_IN1, HIGH);
+        digitalWrite(MOTOR_B_LEFT_IN2, LOW);
+        analogWrite(MOTOR_B_RIGHT_ENABLE,255);
+        digitalWrite(MOTOR_B_RIGHT_IN1, HIGH);
+        digitalWrite(MOTOR_B_RIGHT_IN2, LOW);
+        Serial.println("Moving forword");
+        str_msg.data = hello;
+        chatter.publish( &str_msg );
+    } else if ( v == 0 && w == 0) {
+        analogWrite(MOTOR_F_LEFT_ENABLE, 200);
+        digitalWrite(MOTOR_F_LEFT_IN1, HIGH);
+        digitalWrite(MOTOR_F_LEFT_IN2, HIGH);
+        analogWrite(MOTOR_F_RIGHT_ENABLE, 200);
+        digitalWrite(MOTOR_F_RIGHT_IN1, HIGH);
+        digitalWrite(MOTOR_F_RIGHT_IN2, HIGH);
+        analogWrite(MOTOR_B_LEFT_ENABLE, 200);
+        digitalWrite(MOTOR_B_LEFT_IN1, HIGH);
+        digitalWrite(MOTOR_B_LEFT_IN2, HIGH);
+        analogWrite(MOTOR_B_RIGHT_ENABLE, 200);
+        digitalWrite(MOTOR_B_RIGHT_IN1, HIGH);
+        digitalWrite(MOTOR_B_RIGHT_IN2, HIGH);
+        Serial.println("Stop");
+    } else if (v < 0 && w == 0) {
+        analogWrite(MOTOR_F_LEFT_ENABLE, 200);
+        digitalWrite(MOTOR_F_LEFT_IN1, LOW);
+        digitalWrite(MOTOR_F_LEFT_IN2, HIGH);
+        analogWrite(MOTOR_F_RIGHT_ENABLE, 200);
+        digitalWrite(MOTOR_F_RIGHT_IN1, LOW);
+        digitalWrite(MOTOR_F_RIGHT_IN2, HIGH);
+
+        analogWrite(MOTOR_B_LEFT_ENABLE, 200);
+        digitalWrite(MOTOR_B_LEFT_IN1, LOW);
+        digitalWrite(MOTOR_B_LEFT_IN2, HIGH);
+        analogWrite(MOTOR_B_RIGHT_ENABLE, 200);
+        digitalWrite(MOTOR_B_RIGHT_IN1, LOW);
+        digitalWrite(MOTOR_B_RIGHT_IN2, HIGH);
+        Serial.println("Moving");
+    } else if (w > 0 && v == 0) {
+        analogWrite(MOTOR_F_LEFT_ENABLE,255);
+        digitalWrite(MOTOR_F_LEFT_IN1, HIGH);
+        digitalWrite(MOTOR_F_LEFT_IN2, LOW);
+        analogWrite(MOTOR_F_RIGHT_ENABLE,255);
+        digitalWrite(MOTOR_F_RIGHT_IN1, HIGH);
+        digitalWrite(MOTOR_F_RIGHT_IN2, HIGH);
+        Serial.println("w > 0 && v == 0");
+
+        analogWrite(MOTOR_B_LEFT_ENABLE,255);
+        digitalWrite(MOTOR_B_LEFT_IN1, HIGH);
+        digitalWrite(MOTOR_B_LEFT_IN2, LOW);
+        analogWrite(MOTOR_B_RIGHT_ENABLE,255);
+        digitalWrite(MOTOR_B_RIGHT_IN1, HIGH);
+        digitalWrite(MOTOR_B_RIGHT_IN2, HIGH);
+        Serial.println("w > 0 && v == 0");
+    } else if (w < 0 && v == 0) {
+        analogWrite(MOTOR_F_LEFT_ENABLE,255);
+        digitalWrite(MOTOR_F_LEFT_IN1, HIGH);
+        digitalWrite(MOTOR_F_LEFT_IN2, HIGH);
+        analogWrite(MOTOR_F_RIGHT_ENABLE,255);
+        digitalWrite(MOTOR_F_RIGHT_IN1, HIGH);
+        digitalWrite(MOTOR_F_RIGHT_IN2, LOW);
+        Serial.println("w < 0 && v == 0");
+
+        analogWrite(MOTOR_B_LEFT_ENABLE,255);
+        digitalWrite(MOTOR_B_LEFT_IN1, HIGH);
+        digitalWrite(MOTOR_B_LEFT_IN2, HIGH);
+        analogWrite(MOTOR_B_RIGHT_ENABLE,255);
+        digitalWrite(MOTOR_B_RIGHT_IN1, HIGH);
+        digitalWrite(MOTOR_B_RIGHT_IN2, LOW);
+        Serial.println("w < 0 && v == 0");
     }
-  
+
 }
 
-//ros::Subscriber<std_msgs::String> sub("motors_dir", messageCb );
 ros::Subscriber <geometry_msgs::Twist> sub2("/cmd_vel", roverCallBack);
-
-char hello[13] = "hello world!";
 
 void setup()
 {
-  pinMode(13, OUTPUT);
-  nh.initNode();
-  nh.advertise(chatter);
-  //nh.subscribe(sub);
-  nh.subscribe(sub2);
-  
-  pinMode(MOTOR_LEFT_IN1,OUTPUT);
-  pinMode(MOTOR_LEFT_IN2,OUTPUT);
-  pinMode(MOTOR_LEFT_ENABLE,OUTPUT);
-  pinMode(MOTOR_RIGHT_IN1,OUTPUT);
-  pinMode(MOTOR_RIGHT_IN2,OUTPUT);
-  pinMode(MOTOR_RIGHT_ENABLE,OUTPUT);
+    pinMode(13, OUTPUT);
+    nh.initNode();
+    nh.advertise(chatter);
+    nh.subscribe(sub2);
+    Serial.begin(57600);
 
-  pinMode(20, INPUT);
-  attachInterrupt(digitalPinToInterrupt(20), leftticks, CHANGE);
-  pinMode(21, INPUT);
-  attachInterrupt(digitalPinToInterrupt(21), rightticks, CHANGE);
+    pinMode(MOTOR_F_LEFT_IN1, OUTPUT);
+    pinMode(MOTOR_F_LEFT_IN2, OUTPUT);
+    pinMode(MOTOR_F_LEFT_ENABLE, OUTPUT);
+    pinMode(MOTOR_F_RIGHT_IN1, OUTPUT);
+    pinMode(MOTOR_F_RIGHT_IN2, OUTPUT);
+    pinMode(MOTOR_F_RIGHT_ENABLE, OUTPUT);
+    
+    pinMode(MOTOR_B_LEFT_IN1, OUTPUT);
+    pinMode(MOTOR_B_LEFT_IN2, OUTPUT);
+    pinMode(MOTOR_B_LEFT_ENABLE, OUTPUT);
+    pinMode(MOTOR_B_RIGHT_IN1, OUTPUT);
+    pinMode(MOTOR_B_RIGHT_IN2, OUTPUT);
+    pinMode(MOTOR_B_RIGHT_ENABLE, OUTPUT);
+
+    pinMode(20, INPUT);
+    attachInterrupt(digitalPinToInterrupt(20), leftticks, CHANGE);
+    pinMode(21, INPUT);
+    attachInterrupt(digitalPinToInterrupt(21), rightticks, CHANGE);
 
 }
 
 void loop()
 {
-  //str_msg.data = hello;
-  chatter.publish( &str_msg );
-  nh.spinOnce();
-  delay(500);
-}
-
-void motorsmove(int left_motor_speed , char left_motor_dir ,int right_motor_speed , char right_motor_dir){
-  motordir(left_motor_dir ,right_motor_dir );
-  analogWrite(MOTOR_LEFT_ENABLE,constrain(left_motor_speed,0,255));
-  analogWrite(MOTOR_RIGHT_ENABLE,constrain(right_motor_speed,0,255));
-}
-void motordir(char *left_motor_dir ,char *right_motor_dir){
-  // left motor dir
-  if ( left_motor_dir[0] == 'f'){
-      digitalWrite(MOTOR_LEFT_IN1,HIGH);
-      digitalWrite(MOTOR_LEFT_IN2,LOW); 
-      Serial.println("Left");
-  }else if ( left_motor_dir[0] == 'b'){
-      digitalWrite(MOTOR_LEFT_IN1,LOW);
-      digitalWrite(MOTOR_LEFT_IN2,HIGH);
-      Serial.println("Right");
-  }else if ( left_motor_dir[0] == 's'){   
-      digitalWrite(MOTOR_LEFT_IN1,HIGH);
-      digitalWrite(MOTOR_LEFT_IN2,HIGH);
-      Serial.println("Forword");
-  }else if ( left_motor_dir[0] == 'e'){
-      digitalWrite(MOTOR_LEFT_IN1,LOW);
-      digitalWrite(MOTOR_LEFT_IN2,LOW);
-      Serial.println("Stop");
-  }
-  // right motor dir
-  if ( right_motor_dir[0] == 'f'){
-      digitalWrite(MOTOR_RIGHT_IN1,HIGH);
-      digitalWrite(MOTOR_RIGHT_IN2,LOW);   
-  }else if ( right_motor_dir[0] == 'b'){
-      digitalWrite(MOTOR_RIGHT_IN1,LOW);
-      digitalWrite(MOTOR_RIGHT_IN2,HIGH);   
-  }else if ( right_motor_dir[0] == 's'){   
-      digitalWrite(MOTOR_RIGHT_IN1,HIGH);
-      digitalWrite(MOTOR_RIGHT_IN2,HIGH);
-  }else if ( right_motor_dir[0] == 'e'){
-      digitalWrite(MOTOR_RIGHT_IN1,LOW);
-      digitalWrite(MOTOR_RIGHT_IN2,LOW);
-  }
+    chatter.publish( &str_msg );
+    nh.spinOnce();
+    delay(500);
 }
